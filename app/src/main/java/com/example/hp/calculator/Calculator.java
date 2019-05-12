2e9106390abe12c5a7029e5492fb9a58cfb1e7bc
tolea <rezneacanatolie@gmail.com>
2019-05-12 15:28:13 +0300

    Changes in calculator calculation logic.

--- app/src/main/java/com/example/hp/calculator/Calculator.java
+++ app/src/main/java/com/example/hp/calculator/Calculator.java
@@ -10,12 +10,18 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+
 public class Calculator extends AppCompatActivity {
 
-    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonC, buttonPluse, buttonMinuse, buttonEquals, buttonSPref;
+    Button buttonSPref, buttonEquals, buttonC, button1, button2, button3, button4, button5, button6, button7, button8, button9, button0, buttonPluse, buttonMinuse;
+//    private int[] NumericButton = {R.id.bt0, R.id.bt1, R.id.bt2, R.id.bt3, R.id.bt4, R.id.bt5, R.id.bt6, R.id.bt7, R.id.bt8, R.id.bt9};
+//    private int[] OperatorButton = {R.id.MinuseBt, R.id.PluseBt};
     ImageButton buttonCLC;
-    int nr1, nr2;
-    boolean add, sub, forSharedPref = false;
+    //    double nr1, nr2;
+    boolean forSharedPref ;
     final String Values = "values";
     SharedPreferences sharedPref;
 
@@ -23,6 +29,7 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        forSharedPref = true;
 
 
         String UserName = getIntent().getExtras().getString("UserName");
@@ -54,21 +61,24 @@ public class Calculator extends AppCompatActivity {
         buttonEquals = (Button) findViewById(R.id.EqualsBt);
         buttonSPref = (Button) findViewById(R.id.makeToastSharedPref);
 
+        final String equalsEptss = editText.getText().toString();
+
 
         buttonSPref.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String equalsEpt = editText.getText().toString();
-                if (TextUtils.isEmpty(equalsEpt)) {
+                sharedPref = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(Values, editText.getText().toString());
+                editor.commit();
+                //load values and make toast.
+                String savedText = sharedPref.getString(Values, "");
+                //save values
+
+                if (editor.equals(null)) {
                     Toast.makeText(Calculator.this, "It emty, please calculate something", Toast.LENGTH_SHORT).show();
                 } else {
-                    //save values
-                    sharedPref = getPreferences(MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString(Values, editText.getText().toString());
-                    editor.commit();
-                    //load values and make toast.
-                    String savedText = sharedPref.getString(Values, "");
+
 
                     Toast.makeText(Calculator.this, savedText, Toast.LENGTH_SHORT).show();
                 }
@@ -80,20 +90,17 @@ public class Calculator extends AppCompatActivity {
         buttonEquals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nr1 = Integer.parseInt(editText.getText() + "");
-                forSharedPref = true;
 
-                if (add == true) {
-
-                    editText.setText(nr1 + nr2 + "");
-                    add = false;
-                } else if (sub = true) {
-                    editText.setText(nr1 - nr2 + "");
-                    add = false;
+                String equalsEpt = editText.getText().toString();
+                if (TextUtils.isEmpty(equalsEpt)){
 
                 }
-
-
+                else {
+                    forSharedPref = false;
+                    Expression expression = new ExpressionBuilder(equalsEpt).build();
+                    double result = expression.evaluate();
+                    editText.setText(Double.toString(result));
+                }
             }
         });
 
@@ -122,97 +129,77 @@ public class Calculator extends AppCompatActivity {
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "0");
+                // editText.setText(editText.getText() + "0");
+                editText.append(button0.getText());
             }
         });
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "1");
+//                editText.setText(editText.getText() + "1");
+                editText.append(button1.getText());
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "2");
+                editText.append(button2.getText());
             }
         });
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "3");
+                editText.append(button3.getText());
             }
         });
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "4");
+                editText.append(button4.getText());
             }
         });
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "5");
+                editText.append(button5.getText());
             }
         });
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "6");
+                editText.append(button6.getText());
             }
         });
         button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "7");
+                editText.append(button7.getText());
+
             }
         });
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "8");
+                editText.append(button8.getText());
             }
         });
         button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText() + "9");
+                editText.append(button9.getText());
             }
         });
         buttonPluse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String equalsEpt = editText.getText().toString();
-                if (TextUtils.isEmpty(equalsEpt)) {
-
-                }
-                else if (equalsEpt.matches("\\+")){
-
-                }
-                else {
-                    nr2 = Integer.parseInt(editText.getText() + "");
-                    editText.setText("+");
-                    add = true;
-                }
+                editText.append(buttonPluse.getText());
 
             }
         });
         buttonMinuse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String equalsEpt = editText.getText().toString();
-                if (TextUtils.isEmpty(equalsEpt)) {
-
-                }
-                else if (equalsEpt.matches("-")){
-
-                }
-                else {
-                    nr2 = Integer.parseInt(editText.getText() + "");
-                    editText.setText("-");
-                    add = true;
-
-                }
+                editText.append(buttonMinuse.getText());
 
             }
         });
